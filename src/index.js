06713a3f938e625d6341d2e339c00e8b8599c42f
atrain42 <maxheimer4211@gmail.com>
2022-06-11 19:48:48 -0400

    updated port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const hbs = require("hbs");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //define paths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
